40b9f11532bc25b8e9e0e2cf318de4db1897806f
nino <dankozero111@gmail.com>
2024-10-06 23:37:55 +0300

    [add]: textArea component with debounce

---
+++ src/pages/shared/ui/TextArea/TextArea.tsx
@@ -0,0 +1,36 @@
+import { ChangeEvent, useEffect, useState } from 'react';
+
+import { useDebounce } from '../../lib/useDebounce';
+
+import classes from './textArea.module.css';
+
+type textAreaProps = {
+  onSetText: (text: string | number | boolean) => void;
+  placeholder: string;
+  isDisabled?: boolean;
+};
+
+export const TextArea = (props: textAreaProps) => {
+  const { onSetText, placeholder, isDisabled = false } = props;
+
+  const [value, setValue] = useState('');
+  const debounce = useDebounce({ value, delay: 500 });
+
+  const onSetNewValue = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    setValue(e.target.value);
+  };
+
+  useEffect(() => {
+    if (debounce) onSetText(debounce);
+  }, [debounce, onSetText]);
+
+  return (
+    <textarea
+      placeholder={placeholder}
+      value={value}
+      onChange={onSetNewValue}
+      disabled={isDisabled}
+      className={classes.textArea}
+    />
+  );
+};

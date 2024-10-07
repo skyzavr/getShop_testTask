36ef66609cf070974fbde060dee3b70d36c2be6e
nino <dankozero111@gmail.com>
2024-10-07 22:13:00 +0300

    [feat]: add component that shows list of elements based on list (array) of text

---
+++ src/pages/entities/List/ui/List.tsx
@@ -0,0 +1,17 @@
+import { ListItem } from '@/pages/shared/ui';
+
+import classes from './list.module.css';
+
+type listProps = {
+  list: string[];
+};
+
+export const List = ({ list }: listProps) => {
+  return (
+    <ul className={classes.list}>
+      {list.map((el, i) => (
+        <ListItem text={el} key={i} />
+      ))}
+    </ul>
+  );
+};

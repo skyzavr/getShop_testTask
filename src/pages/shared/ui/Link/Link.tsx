dface4b0c291759b46da29ac85bcdc1e4eb35b4f
nino <dankozero111@gmail.com>
2024-10-07 14:57:53 +0300

    [feat]: update styling with underline propertie

--- src/pages/shared/ui/Link/Link.tsx
+++ src/pages/shared/ui/Link/Link.tsx
@@ -1,8 +1,13 @@
 import classes from './link.module.css';
 type linkProps = {
   children: string;
+  underlineText?: string;
 };
 
-export const Link = ({ children }: linkProps) => {
-  return <a className={classes.link}>{children}</a>;
+export const Link = ({ children, underlineText = 'none' }: linkProps) => {
+  return (
+    <a style={{ textDecoration: underlineText }} className={classes.link}>
+      {children}
+    </a>
+  );
 };

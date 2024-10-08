4ac04f47eed675f25e068a558192954bf3477ecc
nino <dankozero111@gmail.com>
2024-10-08 15:28:54 +0300

    [fix]: add styles based on props

--- src/pages/shared/ui/Link/Link.tsx
+++ src/pages/shared/ui/Link/Link.tsx
@@ -1,12 +1,18 @@
+import classNames from 'classnames';
 import classes from './link.module.css';
 type linkProps = {
   children: string;
+  isActive?: boolean;
   underlineText?: string;
 };
 
-export const Link = ({ children, underlineText = 'none' }: linkProps) => {
+export const Link = (LinkProps: linkProps) => {
+  const { children, isActive = false, underlineText = 'none' } = LinkProps;
+
+  const cl = classNames(classes.link, isActive ? classes.active : '');
+
   return (
-    <a style={{ textDecoration: underlineText }} className={classes.link}>
+    <a style={{ textDecoration: underlineText }} className={cl}>
       {children}
     </a>
   );

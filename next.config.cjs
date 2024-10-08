a287e77edb4f54ff969783193b37eaeea842771c
nino <dankozero111@gmail.com>
2024-10-08 21:42:49 +0300

    [build]

---
+++ next.config.cjs
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+// };
+module.exports = {
+  pageExtensions: ['page.tsx', 'page.ts', 'page.jsx', 'page.js'],
+  reactStrictMode: true,
+};
+// export default nextConfig;

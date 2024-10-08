29018b80ac61b3488a3f5f09b1cd3f1dc6b66c2e
nino <dankozero111@gmail.com>
2024-10-08 20:14:34 +0300

    [feat]:add import of styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import '@/styles/clear.css';
 import '@/styles/globals.css';
 import '@/styles/colors.css';
 import '@/styles/typography.css';
+import '@/styles/transitions.css';
 import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {

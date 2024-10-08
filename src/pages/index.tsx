eab76ea49930d99dbb71a742d28d4daf0c762126
nino <dankozero111@gmail.com>
2024-10-08 20:14:04 +0300

    [feat]: add refs list of every block that tracked to observe

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, createRef } from 'react';
 import Head from 'next/head';
 
 import { Header } from '@widgets/Header';
@@ -10,6 +10,20 @@ export const Home = () => {
   const [activeBlock, setActiveBlock] = useState<string>('');
   const params = { screen: activeBlock, setScreen: setActiveBlock };
 
+  type refs = { [key: string]: { current: HTMLDivElement | null } };
+
+  const refs: refs = screens.reduce((refsObj: refs, { key }) => {
+    refsObj[key] = createRef();
+    return refsObj;
+  }, {});
+
+  const handleCLick = (name: string) => {
+    if (name === '') return;
+    (refs[name].current as HTMLDivElement).scrollIntoView({
+      behavior: 'smooth',
+    });
+  };
+
   return (
     <>
       <Head>
@@ -18,10 +32,13 @@ export const Home = () => {
         <link rel="icon" href="/images/favicon.ico" type="image/x-icon" />
       </Head>
       <Scroll />
-      <Header screen={activeBlock} />
+      <Header screen={activeBlock} navTo={handleCLick} />
       <main>
         {screens.map((screen) => (
-          <ObserverWrapper {...{ ...screen, ...params }} key={screen.key}>
+          <ObserverWrapper
+            {...{ ...screen, ...params, refParam: refs[screen.key] }}
+            key={screen.key}
+          >
             {screen.component}
           </ObserverWrapper>
         ))}

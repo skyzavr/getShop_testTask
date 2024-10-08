3d94fef4e974841d58cf677d0add9d2dff792e8b
nino <dankozero111@gmail.com>
2024-10-08 20:10:40 +0300

    [feat]: api for shared components

---
+++ src/pages/shared/ui/index.ts
@@ -0,0 +1,12 @@
+export { Button } from './Button/Button';
+export { Link } from './Link/Link';
+export { TabsButtonItem } from './TabsButtonItem/TabsButtonItem';
+export { TabsButton } from './TabsButton/TabsButton';
+export { TextArea } from './TextArea/TextArea';
+export { Input } from './Input/Input';
+export { CheckBox } from './CheckBox/CheckBox';
+export { ListItem } from './ListItem/ListItem';
+export { Bullet } from './Bullet/Bullet';
+export { ScrollBtn } from './ScrollBtn/ScrollBtn';
+export { MenuButton } from './MenuButton/MenuButtons';
+export { ObserverWrapper } from './ObserverWrapper/ObserverWrapper';

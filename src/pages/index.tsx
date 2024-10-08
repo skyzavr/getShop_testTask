da3d90955d386f45c87947e854fb7d5f1a56521c
nino <dankozero111@gmail.com>
2024-10-08 18:05:43 +0300

    [feat]: add the main blocks

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,15 @@
+import { useState } from 'react';
 import Head from 'next/head';
 
-const Home = () => {
+import { Header } from '@widgets/Header';
+import { screens } from './api/screens';
+import { Scroll } from '@features/Scroll';
+import { ObserverWrapper } from '@shared/ui';
+
+export const Home = () => {
+  const [activeBlock, setActiveBlock] = useState<string>('');
+  const params = { screen: activeBlock, setScreen: setActiveBlock };
+
   return (
     <>
       <Head>
@@ -8,6 +17,15 @@ const Home = () => {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/images/favicon.ico" type="image/x-icon" />
       </Head>
+      <Scroll />
+      <Header screen={activeBlock} />
+      <main>
+        {screens.map((screen) => (
+          <ObserverWrapper {...{ ...screen, ...params }} key={screen.key}>
+            {screen.component}
+          </ObserverWrapper>
+        ))}
+      </main>
     </>
   );
 };

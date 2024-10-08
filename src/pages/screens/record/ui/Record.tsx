0b176a54e0ac7036094745ff03e8825404180562
nino <dankozero111@gmail.com>
2024-10-08 20:30:47 +0300

    [feat]: add record page/slide

---
+++ src/pages/screens/record/ui/Record.tsx
@@ -0,0 +1,32 @@
+import Image from 'next/image';
+
+import { List } from '@/pages/entities/List';
+import { Button } from '@shared/ui';
+
+import classes from './record.module.css';
+export const Record = () => {
+  const list: string[] = [
+    'Выручка, CTR, показы и другие показатели в реальном времени.',
+    'Инструменты контроля качества трафика.',
+    'Ежемесячные автоматические отчёты для каждого правообладателя.',
+  ];
+
+  return (
+    <section className={classes.container}>
+      <div className={classes.wrapper}>
+        <div className={classes.descriptionWrapper}>
+          <div>
+            <h2 className={classes.header}>
+              Подробные отчёты для вас и правообладателей
+            </h2>
+            <List list={list} />
+          </div>
+          <Button cl="black">Заказать звонок</Button>
+        </div>
+        <div className={classes.reportImg}>
+          <Image src="/images/report.png" alt="" width={721} height={482} />
+        </div>
+      </div>
+    </section>
+  );
+};
